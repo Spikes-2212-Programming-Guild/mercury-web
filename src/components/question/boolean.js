2d914b872ea603ef267795e0ea7bfb13a46f252f
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-01-20 13:42:54 +0000

    added jsdoc to boolean.js

--- src/components/question/boolean.js
+++ src/components/question/boolean.js
@@ -1,6 +1,9 @@
 import SelectQuestion from './enum'
 import React, {Component} from 'react'
 
+/**
+ * This class extends {SelectQuestion}, and it has two built in options, either "Yes" or "No"
+ */
 class SwitchQuestion extends SelectQuestion {
   constructor (props) {
     props.data.params = {}
