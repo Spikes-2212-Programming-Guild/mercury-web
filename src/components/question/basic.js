174f8de377fa2f1e9e2edd5e313c02c644eca63d
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-01-20 13:40:19 +0000

    added jsdoc to basic.js

--- src/components/question/basic.js
+++ src/components/question/basic.js
@@ -1,6 +1,15 @@
 import React, {Component} from 'react'
 
+/**
+ * this class defines basic methods for all types of questions.
+ */
 class Question extends Component {
+  /**
+   * this constructs a new {Question} instance with props.
+   * saves all the relevant props in the state object
+   * @param props
+   * @constructor
+   */
   constructor (props) {
     super(props)
     this.state = {
@@ -9,10 +18,20 @@ class Question extends Component {
     }
   }
 
+  /**
+   * this method returns the name of the question that is optimized to be an HTML property.
+   * @returns {string} the name of the question, optimized to use as an HTML property
+   */
   getOptimizedName () {
     return this.optimizeName(this.state.data.name + this.state.gameStage)
   }
 
+  /**
+   * this methods is used to optimize a name to a valid HTML property format.
+   * the method removes all spaces and makes all the characters lower case.
+   * @param name - the name that should be optimized
+   * @returns {string} - the name in an optimized format
+   */
   optimizeName (name) {
     return name.split(' ').join('').toLowerCase()
   }
