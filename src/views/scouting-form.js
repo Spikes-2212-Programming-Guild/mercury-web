c0f7a4510b4a227fd080d321e06f7112d92c0a14
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-01-22 17:57:45 +0000

    fixed data submition in ScoutingForm

--- src/views/scouting-form.js
+++ src/views/scouting-form.js
@@ -46,7 +46,22 @@ class ScoutingForm extends Component {
       return (
         <div>
           <h1>Scouting Form</h1>
-          <form ref="scouting-form" id="scouting-form">
+          <form ref="scouting-form" onSubmit={(event) => {
+            event.preventDefault()
+            const form = ReactDOM.findDOMNode(this.refs['scouting-form'])
+            const data = {}
+            const elements = Array.from(form.elements)
+            elements.forEach(function (element) {
+              if (element.type === 'radio') {
+                if (element.checked) data[element.name] = element.value
+              } else {
+                data[element.name] = element.value
+              }
+            })
+            axios.post('/scouting-form/submit', {form: data})
+              .then(function () { alert('Submited Data Successfully') })
+              .catch(function () { alert('Error While Submitting Data') })
+          }}>
             <NumericQuestion data={{name: 'Team Number',
               type: 'number',
               params: {
@@ -55,26 +70,8 @@ class ScoutingForm extends Component {
             }} gameStage=""/>
             {questionSets}
 
+            <input type="submit" value="Submit"/>
           </form>
-          <input type="submit" value="Submit" onClick={
-            () => {
-              const form = ReactDOM.findDOMNode(this.refs['scouting-form'])
-
-              const data = {}
-
-              const elements = Array.from(form.elements)
-
-              elements.forEach((element) => {
-                data[element.name] = element.value
-              })
-
-              axios.post('scouting-form/submit', {form: data}, {
-                'Content-Type': 'application/json'
-              })
-                .then(() => alert('Submitted Data Successfully'))
-                .catch(() => alert('Error While Submiting Data'))
-            }
-          }/>
         </div>
       )
     }
