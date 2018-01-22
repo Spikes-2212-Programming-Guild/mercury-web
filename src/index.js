5928dbc27fa1f5a2732ac48b764f5e98c15118f9
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-01-22 10:51:24 +0000

    fixed importing of app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/scouting-form';
+import App from './app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
