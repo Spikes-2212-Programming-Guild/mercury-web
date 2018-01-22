21e2103968e304254256fbe447545e9675b161ec
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-01-22 18:41:46 +0000

    added jsdoc to routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,10 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import ScoutingForm from '../views/scouting-form'
 import HomePage from '../views/home'
 
+/**
+ * This module is responsible for displaying different components depending on the url (emulating a multi page app)
+ */
+
 export default () => (
   <BrowserRouter>
     <Switch>
