0cadba258401012cb6065ed8ead62d02afff2c4e
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-01-22 10:26:24 +0000

    renamed app.js to scouting-form.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import App from './components/scouting-form';
 
 ReactDOM.render(<App />, document.getElementById('root'));
