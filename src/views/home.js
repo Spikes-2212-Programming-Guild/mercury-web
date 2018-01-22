ddc30f343c41140ef76e8a6b8af2367c0b43315a
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-01-22 15:17:35 +0000

    fixed redirection to scouting-form

--- src/views/home.js
+++ src/views/home.js
@@ -1,10 +1,27 @@
-import React, {Componenet} from 'react'
-import Redirect from 'react-router-dom'
-class Home extends Componenet {
-  render () {
-    return (<div>
-      <button onClick={<Redirect to="/scouting-form" push /> }>Scout</button>
+import React, {Component} from 'react'
+import {Redirect} from 'react-router-dom'
+class HomePage extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      redirectToScoutingForm: false
+    }
+
+    this.renderResult = (<div>
+      <h1>Welcome To Mercury!!!</h1>
+      <button onClick={
+        () => {
+          this.renderResult = <Redirect to="/scouting-form" push={true}/>
+          this.forceUpdate()
+        }
+      }>
+        Scout
+      </button>
     </div>)
   }
+
+  render () {
+    return this.renderResult
+  }
 }
-export default Home
+export default HomePage
