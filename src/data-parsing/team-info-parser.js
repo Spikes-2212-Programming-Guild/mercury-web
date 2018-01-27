22567dff1977bd7bbf8ee444966d0af9fb0307f5
Yonatan <yognatan33@gmail.com>
2018-01-27 11:40:31 +0200

    implemented team-info-parser

--- src/data-parsing/team-info-parser.js
+++ src/data-parsing/team-info-parser.js
@@ -1,10 +1,41 @@
 /**
- * @todo implement data parsing
- * @param data
+ * @param info
  * @returns {*}
  */
-function parser (data) {
-  return data
+function parser (info) {
+  const parsedInfo = {}
+  Object.keys(info).forEach(key => {
+    const newQuestion = {
+      'type': info[key].type
+    }
+    if (info[key].options) {
+      newQuestion.options = info[key].options
+    } else if (newQuestion.type === 'boolean') {
+      newQuestion.options = ['Yes', 'No']
+    } else {
+      newQuestion.data = info[key].data
+      parsedInfo[key] = newQuestion
+      return
+    }
+
+    const parsedData = {}
+    newQuestion.options.forEach(option => {
+      parsedData[option] = 0
+    })
+
+    info[key].data.forEach(input => {
+      parsedData[input] += 1
+    })
+
+    const arr = []
+    Object.keys(parsedData).forEach(option => arr.push(parsedData[option]))
+
+    newQuestion.data = arr
+
+    parsedInfo[key] = newQuestion
+  }
+  )
+  return parsedInfo
 }
 
 export default parser
