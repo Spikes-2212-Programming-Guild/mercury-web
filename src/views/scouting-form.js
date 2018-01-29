dff4be0ba1d5e84b4d83e93b6e4edd408dc3711c
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-01-29 20:21:12 +0000

    updated api urls

--- src/views/scouting-form.js
+++ src/views/scouting-form.js
@@ -25,7 +25,7 @@ class ScoutingForm extends Component {
       form: null
     }
 
-    axios.get('/scouting-form/current').then(res => {
+    axios.get('/api/game-config/').then(res => {
       this.setState({form: res.data})
       this.forceUpdate()
     })
@@ -56,7 +56,7 @@ class ScoutingForm extends Component {
                 data[element.name] = element.value
               }
             })
-            axios.post('/scouting-form/submit', {form: data})
+            axios.post('/api/team/submit-match', {match: data})
               .then(function () { alert('Submited Data Successfully') })
               .catch(function () { alert('Error While Submitting Data') })
           }}>
