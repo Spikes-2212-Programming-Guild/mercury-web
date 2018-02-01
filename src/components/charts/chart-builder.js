92ff8ffd6b87ffd0d7cb45dff0c648b1e2c88d72
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-02-01 17:38:52 +0000

    fixed exports in chart-builder

--- src/components/charts/chart-builder.js
+++ src/components/charts/chart-builder.js
@@ -1,10 +1,9 @@
 const colors = ['rgba(46, 139, 87, 0.8)', 'rgba(220, 20, 60, 0.8)', 'rgba(64, 224, 208, 0.8)', 'rgba(192, 192, 192, 0.8)', 'rgba(255, 218, 185, 0.8)', 'rgba(75, 0, 130, 0.8)']
 
-function generateColors (size) {
+export function generateColors (size) {
   const res = []
   for (let i = 0; i < size; i++) {
     res[i] = colors[i % colors.length]
   }
   return res
 }
-
