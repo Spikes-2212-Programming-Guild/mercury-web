c84df87e4e4211501eff1380fb949ce5fc577624
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-02-01 17:34:35 +0000

    added new chart-builder.js file

--- src/components/graph-set.js
+++ src/components/graph-set.js
@@ -8,7 +8,7 @@ class GraphSet extends Component {
     const colors = ['rgba(46, 139, 87, 0.8)', 'rgba(220, 20, 60, 0.8)', 'rgba(64, 224, 208, 0.8)', 'rgba(192, 192, 192, 0.8)', 'rgba(255, 218, 185, 0.8)', 'rgba(75, 0, 130, 0.8)']
     this.generateColors = function (size) {
       const res = []
-      for (var i = 0; i < size; i++) {
+      for (let i = 0; i < size; i++) {
         res[i] = colors[i % colors.length]
       }
       return res
