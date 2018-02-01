f3274771aeab25fe0309190ffd763aff9c8a0c31
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-02-01 17:41:11 +0000

    added new doughnut.js file

---
+++ src/components/charts/doughnut.js
@@ -0,0 +1,23 @@
+import {generateColors} from "./chart-builder"
+import {Doughnut} from 'react-chartjs-2'
+export default (title, chartRecipe) => {
+  const labels = chartRecipe.labels
+  const data = chartRecipe.data
+  return (
+    <Doughnut
+      data={{
+        datasets: [
+          {
+            label: title,
+            borderWidth: 1,
+            data: data,
+            backgroundColor: generateColors(data.length)
+          }
+        ],
+        labels: labels
+      }}
+      height={'10%'}
+      width={'100%'}
+      options={{}}
+    />)
+}
