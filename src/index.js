011e350e4eea66de80094e7f61b939239a30943f
Yonatan <yognatan33@gmail.com>
2018-02-06 19:51:18 +0200

    Added documentation to app and index.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
 
+/**
+ * When the client starts - starts app.
+ */
 ReactDOM.render(<App />, document.getElementById('root'))
