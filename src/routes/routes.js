b1ed734bfe03052f73a2c73ac4a0d1f29df16763
Yonatan <yognatan33@gmail.com>
2018-02-06 15:31:29 +0200

    Redirected '/' to '/scouting-form' in Routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 import ScoutingForm from '../views/scouting-form'
 import TeamInfo from '../views/team-info'
 
@@ -12,7 +12,9 @@ export default () => (
   <BrowserRouter>
     <Switch>
       <Route path="/scouting-form" exact component={ScoutingForm}/>
-      <Route path="/" exact component={ScoutingForm}/>
+      <Route path="/" exact render={() => (
+        <Redirect to="/scouting-form"/>
+      )}/>
       <Route path="/team/:teamNumber" exact component={TeamInfo}/>
     </Switch>
   </BrowserRouter>
