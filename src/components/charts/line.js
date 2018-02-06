e764309901211f59ded914b64b76be91b90efcb5
Yonatan <yognatan33@gmail.com>
2018-02-06 21:40:29 +0200

    Finished doc for line chart. Please look at the comment in line 13

--- src/components/charts/line.js
+++ src/components/charts/line.js
@@ -2,9 +2,15 @@ import React from 'react'
 import {generateColors} from './chart-builder'
 import {Line} from 'react-chartjs-2'
 
+/**
+ * Creates a line chart using parsed information.
+ * @param title - the title of the chart
+ * @param chartRecipe - the parsed data to display
+ * @returns {} The line chart tag.
+ */
 export default (title, chartRecipe) => {
   const data = chartRecipe.data
-  const labels = chartRecipe.labels
+  const labels = chartRecipe.labels // What's this for?
   return (
     <Line
       data={
@@ -14,10 +20,10 @@ export default (title, chartRecipe) => {
             {
               label: title,
               borderWidth: 1,
-              borderColor: this.generateColors(1),
+              borderColor: this.generateColors(data.length),
               data: data,
               fill: false,
-              backgroundColor: this.generateColors(1)
+              backgroundColor: this.generateColors(data.length)
             }
           ]
         }
