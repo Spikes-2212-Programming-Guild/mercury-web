33db17c3bd0788316416f929fb8d583a556fd009
Yonatan <yognatan33@gmail.com>
2018-02-08 10:40:37 +0200

    Finished doc for chart-builder.

--- src/components/charts/chart-builder.js
+++ src/components/charts/chart-builder.js
@@ -1,5 +1,15 @@
+/**
+ * A list of colors to display in charts. Is an array of rgba values.
+ * @type {string[]}
+ */
 const colors = ['rgba(46, 139, 87, 0.8)', 'rgba(220, 20, 60, 0.8)', 'rgba(64, 224, 208, 0.8)', 'rgba(192, 192, 192, 0.8)', 'rgba(255, 218, 185, 0.8)', 'rgba(75, 0, 130, 0.8)']
 
+/**
+ * Gives an array with a requested number of colors.
+ * There are 6 different colors.
+ * @param size - the number of colors requested.
+ * @return {String[]} An array of rgba values.
+ */
 export function generateColors (size) {
   const res = []
   for (let i = 0; i < size; i++) {
