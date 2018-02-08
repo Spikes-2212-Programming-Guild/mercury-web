4eea5facf01cacef438501b05491b7672985c047
Yonatan <yognatan33@gmail.com>
2018-02-08 11:03:06 +0200

    Made prework for making colorblindness support

--- src/components/charts/chart-builder.js
+++ src/components/charts/chart-builder.js
@@ -1,7 +1,16 @@
-const colors = ['rgba(46, 139, 87, 0.8)', 'rgba(220, 20, 60, 0.8)', 'rgba(64, 224, 208, 0.8)', 'rgba(192, 192, 192, 0.8)', 'rgba(255, 218, 185, 0.8)', 'rgba(75, 0, 130, 0.8)']
+const colorSets = {
+  'normal': ['rgba(46, 139, 87, 0.8)', 'rgba(220, 20, 60, 0.8)', 'rgba(64, 224, 208, 0.8)', 'rgba(192, 192, 192, 0.8)', 'rgba(255, 218, 185, 0.8)', 'rgba(75, 0, 130, 0.8)'],
+  'redGreen': [],
+  'GrayScale': []
+}
+
+function getColorList () {
+  return colorSets.normal
+}
 
 export function generateColors (size) {
   const res = []
+  var colors = getColorList()
   for (let i = 0; i < size; i++) {
     res[i] = colors[i % colors.length]
   }
