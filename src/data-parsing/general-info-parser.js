85adbe893c80df4dd650cd99d97956652bc10f1c
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-02-10 15:54:12 +0000

    implemented stage 1 parsing

--- src/data-parsing/general-info-parser.js
+++ src/data-parsing/general-info-parser.js
@@ -1,10 +1,45 @@
 
-function parser (info) {
-  const chartRecipes = {}
-  const keys = Object.keys(info)
-  keys.forEach(teamNumber => {
+const stage1Parsers = {
+  number: (question) => {
+    const result = {}
+    let avg = 0
+    question.data = question.data.map((item) => parseInt(item))
+    question.data = question.data.sort((a, b) => a - b)
+    question.data.map((item) => {
+      avg += item
+    })
+    result.max = question.data[question.data.length - 1]
+    result.avg = avg
+    return result
+  },
+  enum: (question) => {
+    const result = {}
+    question.options.forEach(dataItem => { result[dataItem] = 0 })
+    question.data.forEach(dataItem => {
+      if (result[dataItem]) {
+        result[dataItem] += 1
+      }
+    })
+    return result
+  }
+}
 
+function parseStage1 (info) {
+  const questionInfo = {}
+  Object.keys(info).forEach(teamNumber => {
+    const question = info[teamNumber]
+    if (question.type === 'boolean') {
+      question.options = ['Yes']
+      questionInfo[teamNumber] = stage1Parsers['enum'](question)
+    } else {
+      questionInfo[teamNumber] = stage1Parsers[question.type](question)
+    }
   })
+  return questionInfo
+}
+
+function parser (info) {
+  const questionInfo = parseStage1(info)
 }
 
 export default parser
