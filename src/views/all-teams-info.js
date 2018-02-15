3b1beae96a29c9f157de6dfbc3f6631d0fdf865a
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-02-15 09:12:44 +0000

    made all-teams-info display charts correctly

--- src/views/all-teams-info.js
+++ src/views/all-teams-info.js
@@ -1,20 +1,40 @@
 import React, {Component} from 'react'
+import MainMenu from './main-menu'
+import GraphSet from '../components/graph-set'
 import axios from 'axios'
 
 class AllTeamsInfo extends Component {
   constructor (props) {
     super(props)
-  }
-
-  render () {
+    this.state = {
+      data: undefined
+    }
     axios.get('/api/team/info/all/')
       .then(info => {
-
+        this.setState({data: info.data})
       })
       .catch(err => {
         alert('error is ' + JSON.stringify(err))
       })
-    return (<div></div>)
+  }
+
+  render () {
+    if (this.state.data) {
+      return <div>
+        <MainMenu view="general-info"/><br/>
+        <GraphSet config="" parser={a => {
+          const result = a
+          Object.keys(result).forEach(key => {
+            result[key].type = 'detailedBar'
+            result[key].labels = a.labels
+          })
+          delete result.labels
+          return result
+        }} data={this.state.data}/>
+      </div>
+    } else {
+      return <div>Loading...</div>
+    }
   }
 }
 export default AllTeamsInfo
