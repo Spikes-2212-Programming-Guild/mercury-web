d331331d2155dd919e3868d0fa1ee39d80c7df21
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-02-25 10:28:20 +0000

    added separated team-info-form component

---
+++ src/components/info-hub/team-info-form.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react'
+import {Redirect} from 'react-router-dom'
+
+class TeamInfoForm extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      formRef: null,
+      toRender: <div className="btn btn-group"><form ref={(ci) => {
+        this.setState({formRef: ci})
+      }} onSubmit={(event) => {
+        event.preventDefault()
+        const form = this.state.formRef
+        const elements = Array.from(form.elements)
+        let teamNumber = 0
+        elements.forEach((element) => {
+          if (element.name === 'teamNumber') {
+            teamNumber = element.value
+            console.log('Received TeamNumber: ' + teamNumber)
+          }
+        })
+        if (teamNumber) {
+          this.setState({
+            toRender: <Redirect to={'/team/' + teamNumber} push={true}/>
+          })
+          this.forceUpdate()
+        }
+      }}>
+        <h2>{'Info by team'}</h2>
+        <b>{'Enter Team Number'}</b> <br/>
+        <input min={1} max={10000} className='btn btn-secondary' type='number' name='teamNumber'/>
+        <input type="submit" className="btn btn-secondary" value="Get Info For Team"/>
+      </form></div>
+    }
+  }
+  render () {
+    return this.state.toRnder
+  }
+}
+export default TeamInfoForm
