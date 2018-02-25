575bda07d6a023ff4a957ab7a88e2856f83b74a6
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-02-25 15:16:01 +0000

    added new general-info-form component

---
+++ src/components/info-hub/general-info-form.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import Redirect from 'react-router-dom'
+
+class GeneralInfoForm extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      toRender: <div>
+        <button className="btn btn-secondary" onClick={() => {
+          this.setState({
+            toRender: <Redirect to="/info/all" push={true}/>
+          })
+        }}>General Info</button>
+      </div>
+    }
+  }
+  render () {
+    return this.state.toRender
+  }
+}
+export default GeneralInfoForm
